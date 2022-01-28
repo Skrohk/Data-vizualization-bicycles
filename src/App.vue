c092ff6269cb0cf6b560a031d42170b2a6530da1
Lucas Ruston <46353518+Skrohk@users.noreply.github.com>
2022-01-28 16:11:38 +0100

    Initialize Tailwind with Vue3

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="nav">
+    <h1 class="text-3xl font-bold underline">    Hello world!  </h1>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
