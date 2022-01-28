f63e76a959ea05604319b971570d8795025a1d86
Lucas Ruston <46353518+Skrohk@users.noreply.github.com>
2022-01-28 17:30:23 +0100

    Start sidebar and gloabla architecture

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="sidebar-container">
+    <div></div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Sidebar',
+};
+</script>
+
+<style scoped>
+  #sidebar-container {
+    width: 30%;
+    height: 100vh;
+    position: fixed;
+    z-index: 1;
+    top:0;
+    right: 0;
+    background-color: grey;
+    overflow-x: hidden;
+    margin-top: 50px;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <sidebar/>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import * as d3 from 'd3';
+import Sidebar from '@/components/Sidebar.vue';
 
 @Options({
   components: {
-    HelloWorld,
+    Sidebar,
   },
 })
 export default class Home extends Vue {}
